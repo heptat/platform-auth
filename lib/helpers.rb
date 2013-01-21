e84770b9f2e687b5fabc67f0b87e8abb70a3a570
heptat <heptat@gmail.com>
2013-01-21 11:43:33 +0800

    write token to db

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,7 +3,11 @@
 helpers do
 
   def generate_token(user)
-    user.uid.to_s + '-12345678'
+    # needs to be some random value:
+    token_value = user.uid.to_s + '-12345678'
+    token = Token.new({:value => token_value, :uid => user.uid, :app => 'app.platform', :created_at => DateTime.now})
+    token.save
+    token_value
   end
 
 end
