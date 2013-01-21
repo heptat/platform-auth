c1d8bf864e70bb22d6aa21812120ede009210211
heptat <heptat@gmail.com>
2013-01-21 12:38:06 +0800

    minor update

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,7 +5,7 @@ helpers do
   def generate_token(user)
     # needs to be some random value:
     token_value = user.uid.to_s + '-12345678'
-    token = Token.new({:value => token_value, :uid => user.uid, :app => 'app.platform', :created_at => DateTime.now})
+    token = Token.new({:value => token_value, :uid => user.uid.to_s, :app => 'app.platform', :created_at => DateTime.now})
     token.save
     token_value
   end
