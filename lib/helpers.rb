d2fb76ca015920a697281336887d7e5e35efd45b
heptat <heptat@gmail.com>
2013-01-21 13:17:59 +0800

    make token random

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,10 +1,10 @@
 # helpers.rb
+require 'digest/sha2'
 
 helpers do
 
   def generate_token(user)
-    # TODO needs to be some random value:
-    token_value = user.uid.to_s + '-12345678'
+    token_value = Digest::SHA512.hexdigest(user.uid.to_s + '12345678' + DateTime.now.to_s)
     token = Token.new({:value => token_value, :uid => user.uid.to_s, :app => 'app.platform', :created_at => DateTime.now})
     token.save
     token_value
